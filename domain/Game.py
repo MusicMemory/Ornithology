35ead717f6bb840b1c974accc7f7b15bde7ff12f
javacook <javacook@gmx.de>
2017-07-27 00:54:17 +0100

    bird_repository ist keine Eigenschaft des Games. Das Game verwendet das Repository lediglich

--- domain/Game.py
+++ domain/Game.py
@@ -8,12 +8,13 @@ class Game:
         self.__no_answers = no_answers
         self.__questions = [None for i in range(no_questions)]
         self.__answers = [None for i in range(no_questions)]
-        self.__bird_repository = BirdRepository()
+
+        bird_repository = BirdRepository()
         for q in range(0,no_questions):
             #Liste __questions mit Zahlen (ID) füllen ohne Wiederholung
             while True:
                 bird_id_candidate = r.randint(0,no_birds-1)
-                bird_candidate = self.__bird_repository.get_bird_by_id(bird_id_candidate)
+                bird_candidate = bird_repository.get_bird_by_id(bird_id_candidate)
                 if not bird_id_candidate in self.__questions:
                     #Alle Vögel sollen die Schwierigkeit 1 haben
                     # QUESTION: was wenn weniger vögel da sind als gebraucht?
@@ -30,11 +31,11 @@ class Game:
                 else:
                     is_different = True
                     for a in answer_list:
-                        if self.__bird_repository.get_bird_by_id(bird_id_candidate).get_name() == self.__bird_repository.get_bird_by_id(a).get_name():
+                        if bird_repository.get_bird_by_id(bird_id_candidate).get_name() == bird_repository.get_bird_by_id(a).get_name():
                             is_different = False
                             break;
                     if is_different:
-                        if self.__bird_repository.get_bird_by_id(bird_id_candidate).get_name() != self.__bird_repository.get_bird_by_id(self.__questions[q]).get_name():
+                        if bird_repository.get_bird_by_id(bird_id_candidate).get_name() != bird_repository.get_bird_by_id(self.__questions[q]).get_name():
                             answer_list.append(bird_id_candidate)
             #richtige Antwort zufällig platzieren
             pos_right_answer = r.randint(0,no_answers-1)
