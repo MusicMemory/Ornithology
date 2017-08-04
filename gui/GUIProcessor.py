2ee5b3d1bcb5e885877ddf06ea58bf0e1a7930d7
javacook <javacook@gmx.de>
2017-08-04 21:34:47 +0200

    Überarbeitung: GUI schöner und Refactoring

--- gui/GUIProcessor.py
+++ gui/GUIProcessor.py
@@ -5,21 +5,34 @@ black = (0, 0, 0)
 white = (255, 255, 255)
 red = (200, 0, 0)
 green = (0, 200, 0)
-dodger_blue_4 = (16,78,139)
-
-no_answer_lines = 2
+alertbox_size = (300, 50)
 answer_height = 40
-answer_width = 400
 
 class GUIProcessor:
-    def __init__(self,width,height):
-        self.screen = screen = pygame.display.set_mode((width,height))
+    def __init__(self, width, height, no_answers):
+        self.screen = screen = pygame.display.set_mode((width, height))
         self.width = width
         self.height = height
-        self.margin_left = width // 10;
+        self.no_answer_lines = no_answers // 2
+        self.margin_left = width // 10
+        self.margin_top = height // 10
+        self.margin_right = self.margin_left
+        self.margin_bottom = width // 15
+        self.width_minus_margins  = self.width - self.margin_left - self.margin_right
+        self.height_minus_margins = self.height - self.margin_top - self.margin_bottom
+        self.image_size = (self.width_minus_margins, self.width_minus_margins * 3 // 4)
+        self.image_pos = (self.margin_left, self.margin_top)
+        self.dist_image_to_alertbox = self.height_minus_margins // 30
+        self.dist_alertbox_to_text = self.height_minus_margins // 25
+        self.alertbox_pos = ((self.width - alertbox_size[0]) / 2, self.margin_top + self.image_size[1] + self.dist_image_to_alertbox)
+        self.text_margin_top = self.margin_top + self.image_size[1] + self.dist_image_to_alertbox + alertbox_size[1] + self.dist_alertbox_to_text
+        self.answer_dist = self.width_minus_margins // 5
+        self.answer_width = (self.width_minus_margins - self.answer_dist) // 2
+
         self.font_answer = pygame.font.SysFont('Comic Sans MS', 30)
         self.font_correct = pygame.font.SysFont('Comic Sans MS', 40)
-        self.font_start = pygame.font.SysFont('Comic Sans MS',20)
+        self.font_start = pygame.font.SysFont('Comic Sans MS',30)
+        self.font_header = pygame.font.SysFont('Comic Sans MS', 50)
 
     def clear(self):
         self.screen.fill(black)
@@ -34,46 +47,55 @@ class GUIProcessor:
         self.screen.blit(logo, (self.width // 2 - w, 50))
 
     def set_image(self, image):
-        image_scaled = pygame.transform.scale(image, (600, 400))
+        image_scaled = pygame.transform.scale(image, self.image_size)
         average_color = pygame.transform.average_color(image_scaled)
         self.clear_by_color(average_color)
-        rect = image_scaled.get_rect()
-        #x zentriert
-        w = rect.width//2
-        self.screen.blit(image_scaled, (self.width // 2 - w, 50))
+        self.screen.blit(image_scaled, self.image_pos)
 
     def set_nth_text(self,n,text):
         textsurface = self.font_answer.render(str(n+1) + '. ' + text, True, white)
-        text_margin_top = self.height - no_answer_lines * answer_height - 100
-        text_y = text_margin_top + n // 2 * answer_height
+
+        rest_space_box_to_bottom = self.height - self.margin_bottom - self.text_margin_top
+        answer_height = rest_space_box_to_bottom // self.no_answer_lines
+
+        text_y = self.text_margin_top + n // 2 * answer_height
         if (n % 2 == 0):
             self.screen.blit(textsurface,(self.margin_left, text_y))
         else:
-            self.screen.blit(textsurface,(self.margin_left + answer_width, text_y))
+            self.screen.blit(textsurface,(self.margin_left + self.answer_dist +  self.answer_width, text_y))
 
     def alert(self, trueOrFalse, points):
-        text = "Richtig! +" + str(points) if trueOrFalse else "Falsch!"
+        x = self.alertbox_pos[0]
+        y = self.alertbox_pos[1]
+        text = "Richtig" if trueOrFalse else "Falsch"
         color = green if trueOrFalse else red
-        pygame.draw.rect(self.screen, color, Rect((200,520),(400,70)))
+        pygame.draw.rect(self.screen, color, Rect((x, y), alertbox_size))
         textsurface = self.font_correct.render(text, True, white)
-        self.screen.blit(textsurface,(360, 543))
+        self.screen.blit(textsurface, (self.width / 2 - 45, y + alertbox_size[1] // 2 - 12))
 
-
-    def final(self, sum_points):
+    def show_final_page(self, sum_points):
         self.screen.fill(black)
         text = "Sie haben " + str(sum_points) + " Punkte erreicht."
         textsurface = self.font_correct.render(text, True, white)
-        self.screen.blit(textsurface,(250, 400))
+        self.screen.blit(textsurface,(self.width // 2 - 200, self.height * 2//5))
+        text = "Möchten Sie noch einmal spielen (j/n)?"
+        textsurface = self.font_correct.render(text, True, white)
+        self.screen.blit(textsurface,(self.width // 2 - 260, self.height * 3//5))
 
-    def start(self):
+    def show_start_page(self):
         self.screen.fill(black)
-        text = "Wähle die Schwierigkeit auf der Tastatur."
-        textsurface = self.font_start.render(text,True,white)
-        textsurface.get_rect().center = (100,100)
-        #self.screen.blit(textsurface,(textsurface.get_rect().centerx),150)
-        text = "Schwierigkeiten: 1-3."
-        textsurface = self.font_start.render(text,True,white)
-        self.screen.blit(textsurface,(250,400))
+        self.printTextRel("Ornitologie-Test", self.font_header, white, (0, 0))
+        self.printTextRel("Wählen Sie die Schwierigkeit auf der Tastatur (1 - 3)", self.font_start, white, (0, 20))
 
     def update(self):
         pygame.display.update()
+
+    def printTextRel(self, text, font, color, percentCoord):
+        textsurface = font.render(text, True, color)
+        absCoord = self.percentToAbs(percentCoord)
+        self.screen.blit(textsurface, absCoord)
+
+    def percentToAbs(self, percentCoord):
+        absX = self.margin_left + percentCoord[0] * self.width_minus_margins // 100
+        absY = self.margin_top +  percentCoord[1] * self.height_minus_margins // 100
+        return (absX, absY)
--- main.py
+++ main.py
@@ -6,12 +6,38 @@ from domain.Game import Game
 from domain.ImageRepository import ImageRepository
 from gui.GUIProcessor import GUIProcessor
 
+
+# Wartet darauf, dass eine Taste gedrückt wird, die sich auch in Bezug auf
+# key_map mappen lässt. Falls key_map die Taste enthält, wird der gemappte
+# Wert zurückgegeben.
+def wait_on_key_and_map(key_map):
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            if event.type == pygame.KEYDOWN:
+                try:
+                    return key_map[event.key]
+                except:
+                    None
+        pygame.time.wait(100)
+
+
 pygame.init()
-display_width = 800
-display_height = 800
+display_width = 700
+display_height = 700
 no_questions = 10
 no_answers = 4
-key_map = {
+
+
+key_map_difficulty = {
+    pygame.K_1 : 1,
+    pygame.K_2 : 2,
+    pygame.K_3 : 3,
+}
+
+key_map_answer = {
     pygame.K_1 : 0,
     pygame.K_2 : 1,
     pygame.K_3 : 2,
@@ -19,78 +45,63 @@ key_map = {
     pygame.K_5 : 4,
     pygame.K_6 : 5,
     pygame.K_7 : 6,
-    pygame.K_8 : 7
+    pygame.K_8 : 7,
 }
 
+key_map_again = {
+    pygame.K_j : True,
+    pygame.K_n : False,
+    pygame.K_ESCAPE : False
+}
+
+print("Start of Ornithologie")
 bird_repository = BirdRepository("birds.csv")
 image_repository = ImageRepository("images")
-gui_processor = GUIProcessor(display_width, display_height)
+gui_processor = GUIProcessor(display_width, display_height, no_answers)
 clock = pygame.time.Clock()
-#start:
-gui_processor.start()
-gui_processor.update()
-keypressed = False
-while not keypressed:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            quit()
-        if event.type == pygame.KEYDOWN:
-            try:
-                difficulty = key_map[event.key]
-                difficulty += 1
-                print("keymap",key_map[event.key])
-                print("difficulty",difficulty)
-                if difficulty < 1 & difficulty > 3:
-                    continue
-            except:
-                continue
-            keypressed = True
-#Spiel:
-game = Game(bird_repository.no_birds(),no_questions,no_answers,difficulty)
-for q in range(no_questions):
-    question,answers = game.get_question(q)
-    bird = bird_repository.get_bird_by_id(question)
-    image = image_repository.load_image(bird.get_filename())
-    gui_processor.clear()
-    gui_processor.set_image(image)
-    for a in range(len(answers)):
-        bird_answer = bird_repository.get_bird_by_id(answers[a])
-        gui_processor.set_nth_text(a,bird_answer.get_name())
 
+again = True
+while again:
+    # Startseite mit der Schwierigkeitsauswahl:
+    gui_processor.show_start_page()
     gui_processor.update()
-    clock.tick()
 
-    keypressed = False
-    while not keypressed:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            if event.type == pygame.KEYDOWN:
-                try:
-                    answer = key_map[event.key]
-                    if answer >= no_answers:
-                        continue
-                except:
-                    continue
+    difficulty = wait_on_key_and_map(key_map_difficulty)
+    print("difficulty = ", difficulty)
 
-                keypressed = True
-                if game.is_correct(q,answer):
-                    game.add_points(bird.get_difficulty())
-                    gui_processor.alert(True,bird.get_difficulty())
-                else:
-                    gui_processor.alert(False,0)
+    # Spiel:
+    game = Game(bird_repository.no_birds(),no_questions,no_answers,difficulty)
+    for q in range(no_questions):
+        question,answers = game.get_question(q)
+        bird = bird_repository.get_bird_by_id(question)
+        image = image_repository.load_image(bird.get_filename())
+        gui_processor.clear()
+        gui_processor.set_image(image)
+        for a in range(len(answers)):
+            bird_answer = bird_repository.get_bird_by_id(answers[a])
+            gui_processor.set_nth_text(a,bird_answer.get_name())
 
-            gui_processor.update()
-            pygame.time.wait(500)
-#Final
-gui_processor.final(game.get_points())
-gui_processor.update()
+        gui_processor.update()
+        clock.tick()
+        answer = wait_on_key_and_map(key_map_answer)
 
-keypressed = False
-while not keypressed:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+        if game.is_correct(q,answer):
+            game.add_points(bird.get_difficulty())
+            gui_processor.alert(True, bird.get_difficulty())
+        else:
+            gui_processor.alert(False, 0)
+
+        gui_processor.update()
+        pygame.event.get()
+        pygame.time.wait(500)
+
+    # Punktestand anzeigen:
+    gui_processor.show_final_page(game.get_points())
+    gui_processor.update()
+
+    again = wait_on_key_and_map(key_map_again)
+    print("again = ", again)
+
+pygame.quit()
+sys.exit()
+
